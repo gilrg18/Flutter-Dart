097e98bcbfc7b5a90be3b867661c0f4ac35a4493
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2022-01-25 18:42:45 -0700

    Scaffold and BuildContext

--- counter_app/lib/screens/home_screen.dart
+++ counter_app/lib/screens/home_screen.dart
@@ -2,16 +2,22 @@
 
 import 'package:flutter/material.dart';
 
+//BuildContext lugar donde fue creado el widget,
+//accedes al arbol de widgets con la lupa azul en la barra de herramientas
+//donde inicias/detienes la app.
 //StatelessWidget es el que crea y establece que la clase sea un widget
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    //Center es otro widget
-    return const Center(
-      //Text es otro widget
-      child: Text('Home Screen'),
+    //Solo estos widgets son reconocidos en este Build Context,
+    //cualquier otro build context que este afuera no reconoce estos widgets
+    return const Scaffold(
+      backgroundColor: Colors.lightBlue,
+      body: Center(
+        child: Text('Home Screen'),
+      ),
     );
   }
 }
