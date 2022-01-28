73e2de5797e5f5a7f97a29dcd3cd22dce12aa0f8
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2022-01-28 15:28:17 -0700

    ListTile taps and Appbar style

--- flutter_components_app/lib/screens/listview2_screen.dart
+++ flutter_components_app/lib/screens/listview2_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Listview2Screen extends StatelessWidget {
-  final animes = const ['AoT', 'DoS', 'Naruto', 'DBZ'];
+  final animes = const ['Attack on Titan', 'Demon Slayer', 'Naruto', 'DBZ'];
 
   const Listview2Screen({Key? key}) : super(key: key);
 
@@ -10,12 +10,23 @@ class Listview2Screen extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
           title: const Center(child: Text('Listview Tipo 2')),
+          elevation: 0,
+          backgroundColor: Colors.indigo,
         ),
         //ListView.builder es lo mismo pero no tiene el separatorBuilder:
         body: ListView.separated(
             itemBuilder: (context, index) => ListTile(
-                title: Text(animes[index]),
-                trailing: const Icon(Icons.arrow_forward_ios_outlined)),
+                  title: Text(animes[index]),
+                  trailing: const Icon(
+                    Icons.arrow_forward_ios_outlined,
+                    color: Colors.indigo,
+                  ),
+                  onTap: () {
+                    final anime = animes[index];
+                    print(anime);
+                  },
+                ),
+
             //se pone guion bajo cuando no se utiliza el argumento, dos __ para el segundo argumento
             separatorBuilder: (_, __) => const Divider(),
             itemCount: animes.length));
