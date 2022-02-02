378b1fc348034bde6bd7be16c61f172bbb247c2c
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2022-02-01 18:07:53 -0700

    Model and Routes List

--- flutter_components_app/lib/router/app_routes.dart
+++ flutter_components_app/lib/router/app_routes.dart
@@ -1,8 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_components_app/models/models.dart';
 import 'package:flutter_components_app/screens/screens.dart';
 
 class AppRoutes {
   static const initialRoute = 'home';
+
+  static final menuOptions = <MenuOption>[
+    MenuOption(
+        route: 'home',
+        name: 'Home Screen',
+        screen: const HomeScreen(),
+        icon: Icons.home_max_sharp),
+    MenuOption(
+        route: 'listview1',
+        name: 'Listview Type 1',
+        screen: const Listview1Screen(),
+        icon: Icons.list_alt),
+    MenuOption(
+        route: 'listview2',
+        name: 'Listview Type 2',
+        screen: const AlertScreen(),
+        icon: Icons.list_alt),
+    MenuOption(
+        route: 'alert',
+        name: 'Alertas - Alerts',
+        screen: const Listview1Screen(),
+        icon: Icons.add_alert_outlined),
+    MenuOption(
+        route: 'card',
+        name: 'Tarjetas - Cards',
+        screen: const CardScreen(),
+        icon: Icons.credit_card),
+  ];
 
   static Map<String, Widget Function(BuildContext)> routes = {
     'home': (BuildContext context) => const HomeScreen(),
