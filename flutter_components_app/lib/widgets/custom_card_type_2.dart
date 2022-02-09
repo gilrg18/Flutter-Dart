025d7cb4868d56118d4281186430bdee295b2c3c
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2022-02-08 19:50:19 -0700

    Adjusting image size

--- flutter_components_app/lib/widgets/custom_card_type_2.dart
+++ flutter_components_app/lib/widgets/custom_card_type_2.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_components_app/themes/app_theme.dart';
 
 class CustomCardType2 extends StatelessWidget {
   const CustomCardType2({Key? key}) : super(key: key);
@@ -6,14 +7,25 @@ class CustomCardType2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-        elevation: 10,
+        clipBehavior: Clip.antiAlias,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        elevation: 30,
+        shadowColor: AppTheme.primary.withOpacity(.5),
         child: Column(
-          children: const [
-            FadeInImage(
+          children: [
+            const FadeInImage(
               image: NetworkImage(
                   'https://images.indianexpress.com/2021/06/big-tree-pixabay.jpg'),
               placeholder: AssetImage('assets/jar-loading.gif'),
-            )
+              width: double.infinity,
+              height: 300,
+              fit: BoxFit.cover, //para que la imagen se adapte a su widget
+              fadeInDuration: Duration(milliseconds: 300),
+            ),
+            Container(
+                alignment: AlignmentDirectional.centerEnd,
+                padding: const EdgeInsets.only(right: 20, top: 10, bottom: 10),
+                child: Text('Lorem ipsum xdxd'))
           ],
         ));
   }
