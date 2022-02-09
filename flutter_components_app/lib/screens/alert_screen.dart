cff9960b42f84677cff18125a7f0d176201b9659
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2022-02-08 22:10:40 -0700

    Alerts for both Android and IOS

--- flutter_components_app/lib/screens/alert_screen.dart
+++ flutter_components_app/lib/screens/alert_screen.dart
@@ -1,9 +1,42 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AlertScreen extends StatelessWidget {
   const AlertScreen({Key? key}) : super(key: key);
 
-  void displayDialog(BuildContext context) {
+  void displayDialogIOS(BuildContext context) {
+    showCupertinoDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (context) {
+          return CupertinoAlertDialog(
+            title: const Text('Titulo'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: const [
+                Text('Contenido de la alerta'),
+                SizedBox(height: 10),
+                FlutterLogo(size: 100)
+              ],
+            ),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child:
+                    const Text('Cancelar', style: TextStyle(color: Colors.red)),
+              ),
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text('Ok'),
+              ),
+            ],
+          );
+        });
+  }
+
+  void displayDialogAndroid(BuildContext context) {
     showDialog(
       barrierDismissible: false,
       context: context,
@@ -27,6 +60,10 @@ class AlertScreen extends StatelessWidget {
               onPressed: () => Navigator.pop(context),
               child: const Text('Cancelar'),
             ),
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Ok'),
+            ),
           ],
         );
       },
@@ -42,7 +79,9 @@ class AlertScreen extends StatelessWidget {
           //     primary: Colors.indigo,
           //     shape: const StadiumBorder(),
           //     elevation: 5),
-          onPressed: () => displayDialog(context),
+          onPressed: () => Platform.isAndroid
+              ? displayDialogAndroid(context)
+              : displayDialogIOS(context),
           child: const Padding(
             padding: EdgeInsets.all(15.0),
             child: Text('Mostrar Alerta', style: TextStyle(fontSize: 20)),
