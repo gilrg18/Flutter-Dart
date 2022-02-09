25c13742af7467fff28b5adee1fea56b102d615f
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2022-02-09 14:29:50 -0700

    Widget Container - Animated Screen

--- flutter_components_app/lib/screens/screens.dart
+++ flutter_components_app/lib/screens/screens.dart
@@ -4,3 +4,4 @@ export 'package:flutter_components_app/screens/listview1_screen.dart';
 export 'package:flutter_components_app/screens/listview2_screen.dart';
 export 'package:flutter_components_app/screens/home_screen.dart';
 export 'package:flutter_components_app/screens/avatar_screen.dart';
+export 'package:flutter_components_app/screens/animated_screen.dart';
