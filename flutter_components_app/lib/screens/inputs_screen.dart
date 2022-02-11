546d4a445e4d01e77e48e94589005f16f4ac754b
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2022-02-11 15:54:24 -0700

    DropdownButtonFormField

--- flutter_components_app/lib/screens/inputs_screen.dart
+++ flutter_components_app/lib/screens/inputs_screen.dart
@@ -61,6 +61,23 @@ class InputsScreen extends StatelessWidget {
                   formValues: formValues,
                 ),
                 const SizedBox(height: 30),
+
+                DropdownButtonFormField<String>(
+                  items: const [
+                    DropdownMenuItem(value: 'Admin', child: Text('Admin')),
+                    DropdownMenuItem(
+                        value: 'Superuser', child: Text('Superuser')),
+                    DropdownMenuItem(
+                        value: 'Developer', child: Text('Developer')),
+                    DropdownMenuItem(
+                        value: 'Jr.Developer', child: Text('Jr.Developer')),
+                  ],
+                  onChanged: (value) {
+                    print(value);
+                    formValues['role'] = value ?? 'Admin';
+                  },
+                ),
+                const SizedBox(height: 30),
                 ElevatedButton(
                   child: const SizedBox(
                     width: double.infinity,
@@ -68,7 +85,7 @@ class InputsScreen extends StatelessWidget {
                   ),
                   onPressed: () {
                     //quitar el teclado
-                    FocusScope.of(context).requestFocus(FocusNode());
+                    //FocusScope.of(context).requestFocus(FocusNode());
                     //.currentState! - ! para decirle a dart que confie en ti
                     //que siempre vas a recibir ese valor
                     if (!myFormKey.currentState!.validate()) {
