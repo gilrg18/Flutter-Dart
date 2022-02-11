5e5ad404330e63e722c2930366e1bf58c0243db6
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2022-02-11 15:27:46 -0700

    Widget Form

--- flutter_components_app/lib/screens/inputs_screen.dart
+++ flutter_components_app/lib/screens/inputs_screen.dart
@@ -6,6 +6,17 @@ class InputsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //Keys ayudan a mantener la referencia a los widgets
+    final GlobalKey<FormState> myFormKey = GlobalKey<FormState>();
+
+    final Map<String, String> formValues = {
+      'first_Name': 'Gilberto',
+      'last_name': 'Rangel',
+      'email': 'lol@lol.com',
+      'password': '12345',
+      'role': 'admin'
+    };
+
     return Scaffold(
       appBar: AppBar(
         title: const Center(
@@ -15,30 +26,48 @@ class InputsScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-          child: Column(
-            children: const [
-              CustomInputField(
-                labelText: 'Nombre',
-                hintText: 'Nombre del usuario',
-              ),
-              SizedBox(height: 30),
-              CustomInputField(
-                labelText: 'Apellido',
-                hintText: 'Apellido del usuario',
-              ),
-              SizedBox(height: 30),
-              CustomInputField(
-                labelText: 'Correo',
-                hintText: 'Correo del usuario',
-                keyboardType: TextInputType.emailAddress,
-              ),
-              SizedBox(height: 30),
-              CustomInputField(
-                labelText: 'Contraseña',
-                hintText: 'Contraseña del usuario',
-                obscureText: true,
-              ),
-            ],
+          child: Form(
+            key: myFormKey,
+            child: Column(
+              children: [
+                const CustomInputField(
+                    labelText: 'Nombre', hintText: 'Nombre del usuario'),
+                const SizedBox(height: 30),
+                const CustomInputField(
+                    labelText: 'Apellido', hintText: 'Apellido del usuario'),
+                const SizedBox(height: 30),
+                const CustomInputField(
+                  labelText: 'Correo',
+                  hintText: 'Correo del usuario',
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                const SizedBox(height: 30),
+                const CustomInputField(
+                  labelText: 'Contraseña',
+                  hintText: 'Contraseña del usuario',
+                  obscureText: true,
+                ),
+                const SizedBox(height: 30),
+                ElevatedButton(
+                  child: const SizedBox(
+                    width: double.infinity,
+                    child: Center(child: const Text('Save')),
+                  ),
+                  onPressed: () {
+                    //quitar el teclado
+                    FocusScope.of(context).requestFocus(FocusNode());
+                    //.currentState! - ! para decirle a dart que confie en ti
+                    //que siempre vas a recibir ese valor
+                    if (!myFormKey.currentState!.validate()) {
+                      print('Formulario No Valido');
+                      return;
+                    }
+                    //imprimir valores del formulario
+                    print(formValues);
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
