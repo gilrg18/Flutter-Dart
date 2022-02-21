d9689222193f423750c502d8007421be27b6562d
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2022-02-21 16:43:50 -0700

    Poster and movie title

--- movies_app/lib/screens/details_screen.dart
+++ movies_app/lib/screens/details_screen.dart
@@ -14,6 +14,11 @@ class DetailsScreen extends StatelessWidget {
         slivers: [
           //slivers: como un appbar pero se mueve con el scroll
           _CustomAppBar(),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              _PosterAndTitle(),
+            ]),
+          ),
         ],
       ),
     );
@@ -46,6 +51,51 @@ class _CustomAppBar extends StatelessWidget {
           fit: BoxFit.cover,
         ),
       ),
+    );
+  }
+}
+
+//_ privado, solo se va a usar dentro de este archivo
+class _PosterAndTitle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(children: [
+        ClipRRect(
+          //cliprrect para poder poner un borderradius
+          borderRadius: BorderRadius.circular(20),
+          child: const FadeInImage(
+            placeholder: AssetImage('assets/no-image.jpg'),
+            image: NetworkImage('https://via.placeholder.com/200x300'),
+            height: 200,
+          ),
+        ),
+        const SizedBox(width: 40),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start, //alinear los textos
+          children: [
+            Text('movie.title',
+                style: textTheme.headline5,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2),
+            Text('movie.originalTitle',
+                style: textTheme.subtitle1,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 1),
+            Row(
+              children: [
+                const Icon(Icons.star_outline, size: 15, color: Colors.grey),
+                const SizedBox(width: 5),
+                Text('movie.voteAverage', style: textTheme.caption),
+              ],
+            ),
+          ],
+        ),
+      ]),
     );
   }
 }
