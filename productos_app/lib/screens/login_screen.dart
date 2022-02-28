45d4acb6a4a4891b9a27e5cfffeb8c1694807cc5
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2022-02-28 01:52:21 -0700

    Reuse input decoration

--- productos_app/lib/screens/login_screen.dart
+++ productos_app/lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:productos_app/ui/input_decorations.dart';
 import 'package:productos_app/widgets/widgets.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -29,6 +30,7 @@ class LoginScreen extends StatelessWidget {
                 'Crear una nueva cuenta',
                 style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
               ),
+              const SizedBox(height: 50),
             ],
           ),
         ),
@@ -50,27 +52,40 @@ class _LoginForm extends StatelessWidget {
             TextFormField(
               autocorrect: false,
               keyboardType: TextInputType.emailAddress,
-              decoration: const InputDecoration(
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.deepPurple,
-                  ),
-                ),
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.deepPurple,
-                    width: 2,
-                  ),
-                ),
+              decoration: InputDecorations.authInputDecoration(
                 hintText: 'john.doe@gmail.com',
                 labelText: 'Correo electronico',
-                labelStyle: TextStyle(
-                  color: Colors.grey,
-                ),
-                prefixIcon:
-                    Icon(Icons.alternate_email_sharp, color: Colors.deepPurple),
+                prefixIcon: Icons.alternate_email_sharp,
               ),
             ),
+            const SizedBox(height: 30),
+            TextFormField(
+              autocorrect: false,
+              obscureText: true,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecorations.authInputDecoration(
+                hintText: '*****',
+                labelText: 'Contraseña',
+                prefixIcon: Icons.lock_outline,
+              ),
+            ),
+            const SizedBox(height: 30),
+            MaterialButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+              disabledColor: Colors.grey,
+              elevation: 0,
+              color: Colors.deepPurple,
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+                child: const Text('Ingresar',
+                    style: TextStyle(color: Colors.white)),
+              ),
+              onPressed: () {
+                //TODO: Login form
+              },
+            )
           ],
         ),
       ),
