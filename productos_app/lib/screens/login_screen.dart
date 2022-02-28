8bc83112e832841ee62d08faf0abb83cf496a268
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2022-02-28 01:36:07 -0700

    Login Form

--- productos_app/lib/screens/login_screen.dart
+++ productos_app/lib/screens/login_screen.dart
@@ -20,12 +20,58 @@ class LoginScreen extends StatelessWidget {
                     const SizedBox(height: 10),
                     Text('Login', style: Theme.of(context).textTheme.headline4),
                     const SizedBox(height: 30),
-                    const Text('Formulario')
+                    const _LoginForm(),
                   ],
                 ),
               ),
+              const SizedBox(height: 50),
+              const Text(
+                'Crear una nueva cuenta',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class _LoginForm extends StatelessWidget {
+  const _LoginForm({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Form(
+        //TODO: mantener la referencia al KEY
+        child: Column(
+          children: [
+            TextFormField(
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.deepPurple,
+                  ),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.deepPurple,
+                    width: 2,
+                  ),
+                ),
+                hintText: 'john.doe@gmail.com',
+                labelText: 'Correo electronico',
+                labelStyle: TextStyle(
+                  color: Colors.grey,
+                ),
+                prefixIcon:
+                    Icon(Icons.alternate_email_sharp, color: Colors.deepPurple),
+              ),
+            ),
+          ],
         ),
       ),
     );
