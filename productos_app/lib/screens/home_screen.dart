e45e5972ff674503128fdf528c66b0a836c83712
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2022-03-03 20:04:26 -0700

    Product Card widget for home screen

--- productos_app/lib/screens/home_screen.dart
+++ productos_app/lib/screens/home_screen.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:productos_app/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('HomeScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text('Productos')),
+      ),
+      //listView.builder, crea los widgets cuando esten cerca de entrar a la pantalla
+      //y no los va a mantener todos activos si no estan en pantalla
+
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (BuildContext context, int index) => const ProductCard(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {},
       ),
     );
   }
