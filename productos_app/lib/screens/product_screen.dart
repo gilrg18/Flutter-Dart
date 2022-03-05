f301d011bb05e6abd80671ceb9c48d6c1cc5753b
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2022-03-05 02:06:52 -0700

    Edit Product Screen - part 2

--- productos_app/lib/screens/product_screen.dart
+++ productos_app/lib/screens/product_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:productos_app/ui/input_decorations.dart';
 import 'package:productos_app/widgets/widgets.dart';
 
 class ProductScreen extends StatelessWidget {
@@ -11,13 +12,109 @@ class ProductScreen extends StatelessWidget {
         child: Column(
           children: [
             Stack(
-              children: const [
-                ProductImage(),
+              children: [
+                const ProductImage(),
+                Positioned(
+                  top: 60,
+                  left: 20,
+                  child: IconButton(
+                    icon: const Icon(
+                      Icons.arrow_back_ios_new,
+                      size: 40,
+                      color: Colors.white,
+                    ),
+                    onPressed: () => Navigator.of(context).pop(),
+                  ),
+                ),
+                Positioned(
+                  top: 60,
+                  right: 20,
+                  child: IconButton(
+                    icon: const Icon(
+                      Icons.camera_alt_outlined,
+                      size: 40,
+                      color: Colors.white,
+                    ),
+                    onPressed: () => Navigator.of(context).pop(),
+                  ),
+                ),
               ],
             ),
+            _ProductForm(),
+            const SizedBox(height: 100),
           ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      //heroanimation entre botones
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 5),
+        child: FloatingActionButton(
+          child: const Icon(Icons.save_outlined),
+          onPressed: () {},
         ),
       ),
     );
   }
+}
+
+class _ProductForm extends StatelessWidget {
+  const _ProductForm({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        width: double.infinity,
+        height: 280,
+        decoration: _buildBoxDecoration(),
+        child: Form(
+          child: Column(
+            children: [
+              const SizedBox(height: 10),
+              TextFormField(
+                decoration: InputDecorations.authInputDecoration(
+                  hintText: 'Nombre del producto',
+                  labelText: 'Nombre:',
+                ),
+              ),
+              const SizedBox(height: 30),
+              TextFormField(
+                keyboardType: TextInputType.number, //teclado numerico
+                decoration: InputDecorations.authInputDecoration(
+                  hintText: '\$150',
+                  labelText: 'Precio:',
+                ),
+              ),
+              const SizedBox(height: 30),
+              SwitchListTile.adaptive(
+                value: true,
+                title: const Text('Disponible'),
+                activeColor: Colors.indigo,
+                onChanged: (value) {},
+              ),
+              const SizedBox(height: 30),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  BoxDecoration _buildBoxDecoration() => BoxDecoration(
+          color: Colors.white,
+          borderRadius: const BorderRadius.only(
+            bottomLeft: Radius.circular(25),
+            bottomRight: Radius.circular(25),
+          ),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black.withOpacity(.05),
+                offset: const Offset(0, 5),
+                blurRadius: 5),
+          ]);
 }
