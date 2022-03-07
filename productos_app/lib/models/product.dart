a939fe6f44fb059cc4e5800c1589ebac17de8aac
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2022-03-07 04:13:38 -0700

    Products Model and Service

---
+++ productos_app/lib/models/product.dart
@@ -0,0 +1,39 @@
+//quicktype.io
+// To parse this JSON data, do
+//
+//     final product = productFromMap(jsonString);
+
+import 'dart:convert';
+
+class Product {
+  Product({
+    required this.available,
+    required this.name,
+    this.picture,
+    required this.price,
+  });
+
+  bool available;
+  String name;
+  String? picture;
+  double price;
+
+  //toma un string y genera una instancia de nuestro producto
+  factory Product.fromJson(String str) => Product.fromMap(json.decode(str));
+  //json para mandarlo al servidor
+  String toJson() => json.encode(toMap());
+
+  factory Product.fromMap(Map<String, dynamic> json) => Product(
+        available: json["available"],
+        name: json["name"],
+        picture: json["picture"],
+        price: json["price"].toDouble(),
+      );
+
+  Map<String, dynamic> toMap() => {
+        "available": available,
+        "name": name,
+        "picture": picture,
+        "price": price,
+      };
+}
---
+++ productos_app/lib/services/products_service.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:productos_app/models/models.dart';
+
+class ProductsService extends ChangeNotifier {
+  final String _baseUrl = 'flutter-backend-38f00-default-rtdb.firebaseio.com';
+
+  final List<Product> products = [];
+
+  //Fetch de productos
+}
