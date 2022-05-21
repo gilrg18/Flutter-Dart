f13b92ed2bd21e51cda843333a1b16bd8bd7d07d
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2022-05-21 13:22:57 -0600

    Getting the devices token

--- notificaciones/lib/main.dart
+++ notificaciones/lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:notificaciones/screens/home_screen.dart';
 import 'package:notificaciones/screens/message_screen.dart';
+import 'package:notificaciones/services/push_notifications_service.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  //para inicializar un contexto antes de las push notifications,
+  //si no inicializamos se ondea el pushnotifications y no jala porque no tiene contexto
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await PushNotificationService.initializeApp();
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
--- notificaciones/lib/services/push_notifications_service.dart
+++ notificaciones/lib/services/push_notifications_service.dart
@@ -1,1 +1,20 @@
 //SHA1: 4A:AB:94:AC:4B:9D:52:19:97:00:36:C8:3B:44:12:14:09:FF:C9:41
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class PushNotificationService {
+  //instancia de firebasemessaging
+  static FirebaseMessaging messaging = FirebaseMessaging.instance;
+  static String? token;
+
+  static Future initializeApp() async {
+    //-push notifications-
+    //generar token
+    await Firebase.initializeApp();
+    token = await FirebaseMessaging.instance.getToken();
+    print('token $token');
+
+    //local notifications
+  }
+}
